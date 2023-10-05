314c4510d0eedc1f54aa772a2f63fc0ddad0e403
qkdtjayme <qkdtjayme@tip.edu.ph>
2023-10-05 13:10:10 +0000

    Commiting Module 4 Parsing files to Github

---
+++ sample_parsejson.py
@@ -0,0 +1,10 @@
+import json
+
+x = '{"name": "John", "age": "30", "city": "New York City"}'
+
+y = json.loads(x)
+print("The output of json files is ", y)
+print("Age:", y["age"])
+print("Name:", y["name"])
+print("City:", y["city"])
+
